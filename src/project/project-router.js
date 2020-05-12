ec0a8802509a5d69c96b698f2cc73994e778b137
Peter Nordman <petermnordman@gmail.com>
2020-05-12 13:03:58 -0400

    GET /api/project/:uuid returns the full project object

--- src/project/project-router.js
+++ src/project/project-router.js
@@ -1,8 +1,8 @@
 const express = require('express');
+const { nanoid } = require('nanoid');
 const ProjectService = require('./project-service.js');
 const CategoryService = require('../category/category-service');
 const TaskService = require('../task/task-service');
-const { nanoid } = require('nanoid');
 
 const ProjectRouter = express.Router();
 
@@ -45,15 +45,29 @@ ProjectRouter
                     .then(dbRes => {
                         return project.categories = dbRes;
                     })
-                
+
                 for (let category of project.categories) {
                     await TaskService.getTasksByCategoryId(
                         req.app.get('db'),
                         category.id
                     )
-                    .then(dbRes => {
-                        return category.tasks = dbRes;
-                    })
+                        .then(dbRes => {
+                            const tasks = dbRes;
+                            
+                            // Set up each task's tags as an array
+                            tasks.forEach(task => {
+                                if (!task.tags) {task.tags = []}
+                                else {
+                                    // Because tags are being stored as text in the database,
+                                    // we convert spaces into entity codes for storage
+                                    // and then convert them back on retrieval.
+                                    task.tags = task.tags.split(' ').map(str => str.replace('&#32;', ' '));
+                                }
+                            })
+
+                            category.tasks = tasks;
+                            return;
+                        })
                 }
 
                 return res.status(200).json(project);
