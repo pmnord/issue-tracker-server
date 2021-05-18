3a48e22a9a7333e4ebe10275542752d6eb32c106
Peter Nordman <peternordman@carenexis.com>
2021-05-18 18:21:30 -0400

    postgrator config

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,14 @@
-require('dotenv').config()
+require('dotenv').config();
 
 module.exports = {
-    "migrationDirectory": "migrations",
-    "driver": "pg",
-    "connectionString": (process.env.NODE_ENV === 'test')
-        ? process.env.TEST_DATABASE_URL
-        : process.env.DATABASE_URL,
-    "ssl": !!process.env.SSL, // !! returns the boolean value of its argument
-}
+  migrationDirectory: 'migrations',
+  driver: 'pg',
+  connectionString:
+    process.env.NODE_ENV === 'test'
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
+  //   ssl: !!process.env.SSL, // !! returns the boolean value of its argument
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
