0fbe8dfe96b469c6d9058452d63679bb81119075
Peter Nordman <peternordman@carenexis.com>
2021-05-18 19:05:05 -0400

    postgrator config

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,8 +7,8 @@ module.exports = {
     process.env.NODE_ENV === 'test'
       ? process.env.TEST_DATABASE_URL
       : process.env.DATABASE_URL,
-  //   ssl: !!process.env.SSL, // !! returns the boolean value of its argument
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  //     ssl: !!process.env.SSL, // !! returns the boolean value of its argument
+  //   ssl: {
+  //     rejectUnauthorized: false,
+  //   },
 };
